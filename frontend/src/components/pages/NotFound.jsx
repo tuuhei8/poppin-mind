eb09f7b5728da9edfd4c445d2b49536b4e191334
Tuukka Heiskanen <tuukkaheiskanen5@gmail.com>
2026-03-24 20:16:20 +0200

    Improved NotFound page message.

--- frontend/src/components/pages/NotFound.jsx
+++ frontend/src/components/pages/NotFound.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom"
+
 export default function NotFound() {
   return (
     <section className="page-hero">
@@ -5,6 +7,9 @@ export default function NotFound() {
           <h1 className="page-hero__title">
             404
           </h1>
+          <p className="page-hero__subtitle">
+            The page you are trying to access isn't here, return to  <strong style={ { color: 'darkgreen' } }><Link to="/">homepage</Link></strong>?
+          </p>
         </div>
       </section>
   )
