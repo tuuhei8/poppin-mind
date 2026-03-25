8ee48605d7fccc22686ddbf98a3dd1f4b2f0d015
Matrixman876 <143689828+Matrixman876@users.noreply.github.com>
2026-03-25 18:24:16 +0200

    Delete frontend/src directory

--- frontend/src/services/services.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-const baseUrl = '/api/services'
-
-const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
-}
-
-export default { getAll }
